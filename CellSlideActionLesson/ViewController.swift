9239e91a8ad88e361e851af6020d50abb54ad1b1
HiroUrata <1712026@i.softbank.jp>
2021-08-17 17:44:45 +0900

    tableView 追加

--- CellSlideActionLesson/ViewController.swift
+++ CellSlideActionLesson/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellContentsArray = ["1","2","3","4","5","6","7","8","9","10"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
 }
 
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContentsArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = cellContentsArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+}
